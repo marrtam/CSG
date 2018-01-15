0d997b9122cd670fa5554d2afa8c4ac794f9d0a4
kiper3k <kiper3k@gmail.com>
2018-01-15 01:49:50 +0100

    changes to main function

--- src/csg/CSG.java
+++ src/csg/CSG.java
@@ -23,6 +23,22 @@ public class CSG {
         // TODO code application logic here
         File bmpFile = new File("bmpImage.bmp");
         BufferedImage image = ImageIO.read(bmpFile);
+        
+        
+//        BufferedImage image = null;
+        File f = null;
+        int width = 963;    //width of the image
+        int height = 640;   //height of the image
+    
+        //read image
+        try{
+          f = new File("D:\\Image\\Taj.jpg"); //image file path
+          image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+          image = ImageIO.read(f);
+          System.out.println("Reading complete.");
+        } catch(IOException e){
+          System.out.println("Error: "+e);
+        }
     }
     
 }
