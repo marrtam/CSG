52db830db470c710802e62910a48e2c39d28c35b
kiper3k <kiper3k@gmail.com>
2018-01-21 18:05:30 +0100

    read and write image

--- src/csg/CSG.java
+++ src/csg/CSG.java
@@ -21,22 +21,34 @@ public class CSG {
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        File bmpFile = new File("bmpImage.bmp");
-        BufferedImage image = ImageIO.read(bmpFile);
+//        File bmpFile = new File("bmpImage.bmp");
+//        BufferedImage image = ImageIO.read(bmpFile);
         
         
 //        BufferedImage image = null;
         File f = null;
-        int width = 963;    //width of the image
-        int height = 640;   //height of the image
-    
+        BufferedImage image = null;
+            
+        int width = 640;    //width of the image
+        int height = 400;   //height of the image
+
         //read image
         try{
-          f = new File("D:\\Image\\Taj.jpg"); //image file path
+          f = new File("resources/input.jpg"); //image file path
           image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
           image = ImageIO.read(f);
           System.out.println("Reading complete.");
-        } catch(IOException e){
+        }catch(IOException e){
+          System.out.println("Error: "+e);
+        }
+
+        //write image
+        try{
+          f = new File("resources/output.jpg");  //output file path
+//          image = new BufferedImage(width, height);
+          ImageIO.write(image, "jpg", f);
+          System.out.println("Writing complete.");
+        }catch(IOException e){
           System.out.println("Error: "+e);
         }
     }
